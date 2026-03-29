2b0b8a4051a910bef6d8982840d0a90605c20eec
bipulchhetri <bipulchhetri80@gmail.com>
2026-03-29 11:51:53 +0530

    tour card final

--- my-app/src/data/package.js
+++ my-app/src/data/package.js
@@ -5,39 +5,42 @@ const packages = [
       slug: "darjeeling-deluxe-stay",
       title: "Darjeeling Deluxe Stay",
       price: 1200,
-      images: ["/images/icons.svg"]
-    },
-    {
-      id: "pkg-2",
-      slug: "goa-family-vacation",
-      title: "Goa Family Vacation",
-      price: 2000,
-      images: [
-        "jhkj",
-        "https://picsum.photos/600/400?5",
-        "https://picsum.photos/600/400?6"
+      images: ["https://northbengaltourism.com/images/holiday/nbthp0071_1.webp",
+        "https://trekinsikkim.in/_next/image?url=https%3A%2F%2Fupload.trekinsikkim.in%2Fuploads%2Fmedia-gallery%2Ffiles-1748685567719-977695027&w=3840&q=75",
+        "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcReFtEgqdgI5sVVieAx9FaINtXiwLDl00IMRg&s"
       ]
     },
     {
       id: "pkg-2",
-      slug: "goa-family-vacation",
-      title: "Goa Family Vacation",
+      slug: "Kalimpong-tour",
+      title: "Kalimpong Tour",
       price: 2000,
       images: [
-        "jhkj",
-        "https://picsum.photos/600/400?5",
-        "https://picsum.photos/600/400?6"
+        "https://assets.traveltriangle.com/blog/wp-content/uploads/2016/08/Untitled-design-183.jpg",
+        "https://s7ap1.scene7.com/is/image/incredibleindia/lord-buddha-statue-kalimpong-west-bengal-city-1-hero?qlt=82&ts=1726645267006",
+        "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTbd6XFxLU-sWfHMxWqpb62otZJSzWargcjnQ&s"
       ]
     },
     {
-      id: "pkg-2",
-      slug: "goa-family-vacation",
-      title: "Goa Family Vacation",
+      id: "pkg-3",
+      slug: "sikkim-family-vacation",
+      title: "Sikkim Family Vacation",
       price: 2000,
       images: [
-        "jhkj",
-        "https://picsum.photos/600/400?5",
-        "https://picsum.photos/600/400?6"
+        "https://dynamic-media-cdn.tripadvisor.com/media/photo-o/15/33/fc/fb/sikkim.jpg?w=1400&h=1400&s=1",
+        "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcR6JOHJpEoDML0Joy9UQvtuGTs8reM7PGoJmw&s",
+        "https://northbengaltourism.com/images/offbeat/anden_1.webp"
+      ]
+    },
+    {
+      id: "pkg-4",
+      slug: "Mirik",
+      title: "Explore Mirik ",
+      price: 2000,
+      images: [
+        "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcR3XNkJvBp9_EyAWCEyV2ZNEP-mfULcnMTHMw&s",
+        "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcT1HbWKMYIGD03N9AdypxqzFie0AyDkjA7gnw&s",
+        "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQDLBSkB2sOYUKG-W_PPx63HFHd1aKBtS7yqw&s"
       ]
     }
   ];
